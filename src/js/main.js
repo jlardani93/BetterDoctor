71a1485f0359afacbecbb6157427d31bfd3edfa9
Justin Lardani <jlardani93@gmail.com>
2018-03-23 13:28:43 -0700

    finishes main.js script and creates html for website.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,42 @@
 import $ from 'jquery';
 var Promise = require('es6-promise-polyfill').Promise;
-import { getDoctors } from './betterDoctorApi.js'; 
+import { getDoctors } from './betterDoctorApi.js';
+
+
+$(document).ready(function(){
+
+  $("#getDoctors").click(function(){
+
+    let userInput = $("#input").val();
+
+    getDoctors(userInput)
+
+    .then(function(response){
+
+      let doctors = JSON.parse(response);
+
+      doctors.data.forEach(function(doctor){
+
+        let doctorName = doctor.practices[0].name;
+        let doctorImageUrl = doctor.profile.image_url;
+        let doctorAddress = doctor.practices[0].visit_address.street + ", " + doctor.practices[0].visit_address.street;
+        let doctorPhone = doctor.practices[0].phones[0].number;
+        let doctorNewPatients = doctor.practices[0].accepts_new_patients;
+        let doctorWebsite = doctor.practices[0].website;
+
+        $("#responseRow").append(
+         `<div class="col-md-3">
+            <div class="doctorCard">
+              <h2 class="doctorName">${doctorName}</h2>
+              <img class="doctorImage" src="${doctorImageUrl}">
+              <h3 class="doctorAddress">Address: ${doctorAddress} </h3>
+              <h3 class="doctorPhone">Phone Number: ${doctorPhone} </h3>
+              <h3 class="doctorNewPatients">Accepting New Patients: ${doctorNewPatients}</h3>
+              <h3 class="doctorWebsite"><a href="${doctorWebsite}">doctor's website</a></h3>
+            </div>
+          </div>`)
+      });
+    });
+  });
+
+})
